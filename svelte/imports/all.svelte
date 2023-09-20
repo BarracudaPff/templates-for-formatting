3fd0f2aa8089c23573964d8e61385add86967cfd
BarracudaPff <mrm.kikll@gmail.com>
2023-09-20 13:43:56 +0200

    add svelte

---
+++ svelte/imports/all.svelte
@@ -0,0 +1,59 @@
+<!-- copied cases from SvelteCreateImportTest -->
+<script>
+    |import {exported} from "./utils";|
+</script>
+<script>
+    |import {
+        A,
+        B,
+    } from "./utils";|
+</script>
+<script>
+    |import {exported} from "./utils";|
+
+    let existingVariable = 5;
+</script>
+<script>
+    import {exported} from "./utils";
+    |import {exported} from "./utils";|
+
+    let existingVariable = 5;
+
+    exported;
+</script>
+<script lang="ts">
+    |import {exported} from "./utils";|
+
+    let existingVariable = 5;
+
+    exported
+</script>
+<script context="module">
+    let existingVariable = 5;
+</script>
+<script>
+    |import {exported} from "./utils";|
+</script>
+<script context="module">
+    |import {exported} from "./utils";|
+
+    exported
+</script>
+<script>
+    let existingVariable = 5;
+</script>
+<script>
+    |import Hello from "./Hello.svelte";|
+</script>
+<Hello />
+import Hello from "./Hello.svelte";
+export {Hello as RenamedHello};
+<script>
+  |import Hello, {something} from "./Hello.svelte";|
+</script>
+<Hello />
+{#each list as l}
+    <script>
+        |import Hello, {something} from "./Hello.svelte";|
+    </script>
+{/each}
